085d6b8e4d336b44b0e3ef165dc2561a076464da
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-08-25 19:08:34 -0500

    Code cleaned up need to work on resetButton

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+// object containing information of each player
 blue = {
     inside: 4,
     outside: 0,
@@ -36,6 +36,7 @@ green = {
     zone: "zone4"
 }
 
+// returns the starting position of each player when called
 function getPosition() {
     switch (player.name) {
         case ("blue"):
@@ -51,6 +52,7 @@ function getPosition() {
     }
 }
 
+// function returns a dice randon dice value
 function rollDice() {
     const diceOption = [1, 2, 3, 4, 5, 6]
     let dice = diceOption[Math.floor(Math.random() * 6)]
@@ -60,6 +62,7 @@ function rollDice() {
 // starting player
 let player = blue
 
+// changes player 
 function changePlayer() {
     if (player == blue)
         player = red
@@ -71,67 +74,66 @@ function changePlayer() {
         player = blue
 }
 
+// 
 function bringOut(index){
     if(player.position[index] == null){
+        // updates players information 
         player.inside --
-        console.log(`bringing Token ${index} out in position ${player.position[index]}`)
+        player.outside ++
+        // adds an class to token onBoard to reduces size of token
         document.getElementById(`${player.zone}Token${index}`).classList.add("onBoard")
+        // receives starting position of token
         let getHomePosition = getPosition()
-        console.log(`placing token ${index} in position ${getHomePosition}`)
         let targetBox = document.getElementById(`box${getHomePosition}`)
+        // moves token position
         targetBox.append(document.getElementById(`${player.zone}Token${index}`))
+        // updates players information 
         player.position[index]=getHomePosition
     }
 }
 
 function moveToken(index,value){
- savedPlayer = player
-if(savedPlayer.position[index] != null){
+ currentPlayer = player
+if(currentPlayer.position[index] != null){
     console.log(`moving Token ${index} by  ${value} `)
-    result = getTokensPath(savedPlayer,index,value)
+    result = getTokensPath(currentPlayer,index,value)
     
     if(result != 100){
-    if(savedPlayer.name != "blue"){
-        if(result<savedPlayer.position[index]){
-            savedPlayer.position[index] = result
-    if(savedPlayer.position[index] != "finished"){
-                let targetBox = document.getElementById(`box${savedPlayer.position[index]}`)
-                console.log(targetBox)
-                targetBox.append(document.getElementById(`${savedPlayer.zone}Token${index}`))}
-    }
-    }
-
-        let autoMove = setInterval(function(){
-            console.log(savedPlayer,savedPlayer.position[index])
-        if( result > savedPlayer.position[index]){
-            savedPlayer.position[index]++
-            targetBox = document.getElementById(`box${savedPlayer.position[index]}`)
+    if(currentPlayer.name != "blue"){
+        if(result<currentPlayer.position[index]){
+            currentPlayer.position[index] = result
+            if(currentPlayer.position[index] != "finished"){
+            let targetBox = document.getElementById(`box${currentPlayer.position[index]}`)
             console.log(targetBox)
-            targetBox.append(document.getElementById(`${savedPlayer.zone}Token${index}`))
+            targetBox.append(document.getElementById(`${currentPlayer.zone}Token${index}`))
+        }
+    }
+    }
+    let autoMove = setInterval(function(){
+         console.log(currentPlayer,currentPlayer.position[index])
+        if( result > currentPlayer.position[index]){
+            currentPlayer.position[index]++
+            targetBox = document.getElementById(`box${currentPlayer.position[index]}`)
+            console.log(targetBox)
+            targetBox.append(document.getElementById(`${currentPlayer.zone}Token${index}`))
         }else clearInterval(autoMove);
     },100)}
     else {
-    // A win Case
-    
     document.getElementById(`${player.zone}Token${index}`).remove()
-    savedPlayer.finished++
-    if(savedPlayer.finished ==4){
-        console.log("won")
+    currentPlayer.finished++
+    if(currentPlayer.finished ==4){
         winner = true
-        setTimeout(()=>{document.getElementById("display").innerHTML=` ${savedPlayer.name} Wins! All 4 ${savedPlayer.name}tokens has crossed the finish line`,1200})
-        
+        setTimeout(()=>{document.getElementById("display").innerHTML=` ${currentPlayer.name} Wins! All 4 ${currentPlayer.name}tokens has crossed the finish line`,1200})    
     }
 }
     }
 }
 
-// function canEat(index,target){
-//     if
-// }
-
+// An algorithm to go round the board
+// still a shaky one 
 function compare(savedPlayer,index,value,v1,v2,v3,v4){
     if(savedPlayer.position[index]+value >v1 ){
-        setTimeout(()=>{document.getElementById("display").innerHTML=` A ${savedPlayer.name} token has crossed the finish line`,200})
+        setTimeout(()=>{document.getElementById("display").innerHTML=` A ${savedPlayer.name} token has crossed the finish line`,10})
         return savedPlayer.position[index] =100
     }
     else if((savedPlayer.position[index] +value) >v2){
@@ -145,6 +147,10 @@ function compare(savedPlayer,index,value,v1,v2,v3,v4){
     else return savedPlayer.position[index]+value
 }
 
+// An algorithm to go round the board
+// I used numbers to save each boxes' path 
+// so this helps track where players are meant to be 
+// not the best logic but would edit soon
 function getTokensPath(savedPlayer,index,value){
     switch (player.name) {
         case ("blue"):
@@ -152,11 +158,10 @@ function getTokensPath(savedPlayer,index,value){
                 if(savedPlayer.finished ==4){
                     setTimeout(()=>{document.getElementById("display").innerHTML=` ${savedPlayer.name} Wins! All 4 ${savedPlayer.name}tokens has crossed the finish line`,200})
                 }
-                setTimeout(()=>{document.getElementById("display").innerHTML=` A ${savedPlayer.name} token has crossed the finish line`,200})
+                setTimeout(()=>{document.getElementById("display").innerHTML=` A ${savedPlayer.name} token has crossed the finish line`,10})
                 return savedPlayer.position[index] =100
             }
             else return savedPlayer.position[index]+value
-            // for going round the div
         case ("red"):
             return compare(savedPlayer,index,value,61,51,12,56)
         case ("green"):
@@ -168,67 +173,80 @@ function getTokensPath(savedPlayer,index,value){
     }
 }
 
+// To avoid other tokens from making moves for who plays 
 function colorCheck(e){
     return e.target.classList[0] ==`${player.name}Token`
     } 
 
+// dice function
+// gameOn, startButton, winner are what I used to stop Listeners from going crazy
 let diceButton 
-let dice1
-let dice2
-let dice1Used
-let dice2Used
+let dice
+let diceUsed
 let gameOn =false
 let startButton = true
 let winner = false
 function playersRoll(){
     diceButton = document.getElementById("roll")
+    resetButton = document.getElementById("reset")
     document.getElementById("start").addEventListener("click",function(){
         if(startButton == true && winner==false){
-        console.log("here")
-        startButton =false
-        setTimeout(()=>{document.getElementById("display").innerHTML=`Let the Game Begin,`},100)
-        setTimeout(()=>{document.getElementById("display").innerHTML="Blue's Starts ... Roll Dice"},1200)
-        diceButton.addEventListener("click",function(){
-        if(gameOn == false){
-            gameOn =true
-            dice1= rollDice()
-            dice2= rollDice()
-            dice1Used = false
-            dice2Used = false
-            console.log(dice1,dice2)
-            setTimeout(()=>{document.getElementById("display").innerHTML=` ${player.name} has rolled a ${dice1} `},400)
-            tokenMoves()
-        }
-    })}})
+            console.log("here")
+            startButton =false
+            setTimeout(()=>{document.getElementById("display").innerHTML=`Let the Game Begin,`},100)
+            setTimeout(()=>{document.getElementById("display").innerHTML="Blue's Starts ... Roll Dice"},1200)
+            // Roll eventListener
+            diceButton.addEventListener("click",function(){
+            if(gameOn == false){
+                gameOn =true
+                dice= rollDice()
+                dice2= rollDice()
+                diceUsed = false
+                dice2Used = false
+                console.log(dice,dice2)
+                setTimeout(()=>{document.getElementById("display").innerHTML=` ${player.name} has rolled a ${dice} `},400)
+                tokenMoves()
+            }})
+            // Reset Event Listener
+            resetButton.addEventListener("click",function(){
+                 gameOn =false
+                 startButton = true
+                 winner = false
+                 dice =0
+                 for(let i =0;i<4;i++){
+                    let targetBox = document.getElementById(`${player.zone}`)
+                    for(let j =1; j<5; j++){
+                        targetBox.append(document.getElementById(`${player.zone}Token${i}`))
+                    }
+                    changePlayer()
+                 }
+
+            })
+         }
+    })
 }
 
 function tokenMoves(){
-        if(dice1 == 6){
+    if(dice == 6){
+        // adds eventListener to token 
             for(let i=1;i<5;i++){
                  document.getElementById(`${player.zone}Token${i}`).addEventListener("click",function(e){
-                    if (dice1Used == false && player.position[i] == null && colorCheck(e) && winner ==false) {
-                        console.log(e.target.classList[0] ==`${player.name}Token`)
+                    if (diceUsed == false && player.position[i] == null && colorCheck(e) && winner ==false) {
                         setTimeout(()=>{document.getElementById("display").innerHTML=`${player.name}'s Turn ... Roll1 Dice`},200)
-                        console.log("here")
                         gameOn =false
                         bringOut(i)
-                        dice1Used = true
+                        diceUsed = true
                         changePlayer()
                         start()
                     }
                  })
                  document.getElementById(`${player.zone}Token${i}`).addEventListener("click",function(e){
-                    if (dice1Used == false && player.position[i] != null && colorCheck(e) && winner ==false) {
-                        console.log(e.target.zone)
-                       
-                        
-                        console.log("here")
-                        moveToken(i,dice1)
+                    if (diceUsed == false && player.position[i] != null && colorCheck(e) && winner ==false) {
+                        moveToken(i,dice)
                         if(winner ==false){
                         setTimeout(()=>{document.getElementById("display").innerHTML=`${player.name}'s Turn ... Roll2 Dice`},200)
-                        console.log("here")
                         gameOn =false
-                        dice1Used = true
+                        diceUsed = true
                         setTimeout(()=>{dice2Used =false},200) 
                         changePlayer()
                         start()}
@@ -237,48 +255,33 @@ function tokenMoves(){
             }
             }else{
             if(player.inside != 4){
-            dice1Used =false
+            diceUsed =false
             for(let i =1;i<5;i++){
                 document.getElementById(`${player.zone}Token${i}`).addEventListener("click",function(e){
-                if (dice1Used == false && player.position[i] != null && colorCheck(e) && winner==false) {
+                if (diceUsed == false && player.position[i] != null && colorCheck(e) && winner==false) {
                     
-                    moveToken(i,dice1)
-                    console.log("here")
-                    // gameOn =false
+                    moveToken(i,dice)
                     if(winner ==false){
                     setTimeout(()=>{document.getElementById("display").innerHTML=`${player.name} Turn ... Roll3 Dice`},200)
-                    dice1Used = true
+                    diceUsed = true
                     setTimeout(()=>{dice2Used =false},200) 
-                     changePlayer()
+                    changePlayer()
                     start()}
                 }
-               
              })
             }}
              else{
                 if(winner ==false){
                     let previousPlayer =player.name
-                setTimeout(()=>{
-                start()
-                changePlayer()
-                console.log("send forth")
-                document.getElementById("display").innerHTML=`${previousPlayer}'s turn was Skipped ... it's ${player.name}'s Turn`
-                gameOn =false
-                
+                    setTimeout(()=>{
+                    start()
+                    changePlayer()
+                    document.getElementById("display").innerHTML=`${previousPlayer}'s turn was Skipped ... it's ${player.name}'s Turn`
+                    gameOn =false
                 },2200)
-                }
-                
-   
-
             }
         }
-}
-function noMove(){
-    changePlayer()
-    start()
-    startButton = true
-    gameOn =false
-
+        }
 }
 function start(){
     playersRoll()
@@ -286,19 +289,3 @@ function start(){
 document.addEventListener("DOMContentLoaded",function(){
    start()
 })
-
-
-
-
-
-
-// for(let i =1;i<5;i++){
-//         document.getElementById(`${player.zone}Token${i}`).addEventListener("click",function(){
-//             bringOut(i)
-//          })
-//     }
-//     for(let i =1;i<5;i++){
-//         document.getElementById(`${player.zone}Token${i}`).addEventListener("click",function(){
-//             moveToken(i,3)
-//          })
-//     }
